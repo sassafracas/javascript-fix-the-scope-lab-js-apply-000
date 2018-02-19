9a09ee9486b20db82ff11e7c54a5f2ad8efe87cc
Aleksandr Roslyakov <torpid@gmail.com>
2018-02-19 18:36:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
+  var theFunk = funkyFunction()
   return function() {
-    var theFunk = funkyFunction()
-    return "FUNKY!"
+  return "FUNKY!"
   }
 }
 
